6473f3922b77d55747f1ce9aff577725d175bd72
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-09-04 02:16:03 +0530

    all dashboards updated

--- Website/public/dash.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.item').forEach(item => {
-    item.addEventListener('mouseenter', () => {
-        item.style.boxShadow = '0 8px 16px rgba(0, 0, 0, 0.2)';
-    });
-
-    item.addEventListener('mouseleave', () => {
-        item.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
-    });
-});
--- Website/public/script.js
+++ Website/public/script.js
@@ -62,12 +62,3 @@ document.addEventListener("DOMContentLoaded", function() {
     handleUserTypeChange();
 });
 
-document.querySelectorAll('.item').forEach(item => {
-    item.addEventListener('mouseenter', () => {
-        item.style.boxShadow = '0 8px 16px rgba(0, 0, 0, 0.2)';
-    });
-
-    item.addEventListener('mouseleave', () => {
-        item.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
-    });
-});
