7ce540ccc7fb6c57c8bbccf0f7d41566ec4506b9
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-09-03 23:23:14 +0530

    farmer dashboard

--- Website/public/script.js
+++ Website/public/script.js
@@ -61,3 +61,13 @@ document.addEventListener("DOMContentLoaded", function() {
     // Call the function initially to set the default state
     handleUserTypeChange();
 });
+
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        item.style.boxShadow = '0 8px 16px rgba(0, 0, 0, 0.2)';
+    });
+
+    item.addEventListener('mouseleave', () => {
+        item.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)';
+    });
+});
