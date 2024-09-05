dc1412e098a786d444052236f5549561ce7a85b4
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-09-06 02:54:06 +0530

    final commit
    flask setup done

--- Website/stock_market/app.py
+++ Website/stock_market/app.py
@@ -6,7 +6,54 @@ s = Stock()
 
 @app.route('/')
 def index():
-    return render_template('index.html', current_price=s.price, transactions=[])
+    return render_template('index.html')
+
+@app.route('/register.html')
+def register():
+    return render_template('register.html')
+
+@app.route('/FinanceIndex.html')
+def finance_index():
+    return render_template('FinanceIndex.html', current_price=s.price, transactions=[])
+
+@app.route('/marketLogin.html')
+def market_login():
+    return render_template('marketLogin.html')
+
+@app.route('/marketplace.html')
+def marketplace():
+    return render_template('marketplace.html')
+
+@app.route('/Invoice.html')
+def invoice():
+    return render_template('Invoice.html')
+
+@app.route('/Equipment.html')
+def equipments():
+    return render_template('Equipment.html')
+@app.route('/farmerLogin.html')
+def farmer_login():
+    return render_template('farmerLogin.html')
+
+@app.route('/farmerDashboard.html')
+def farmer_dashboard():
+    return render_template('farmerDashboard.html')
+
+@app.route('/farmerProfile.html')
+def farmer_profile():
+    return render_template('farmerProfile.html')
+
+@app.route('/Fertilizers.html')
+def fertilizers():
+    return render_template('Fertilizers.html')
+
+@app.route('/itemsBought.html')
+def item_bought():
+    return render_template('itemsBought.html')
+
+@app.route('/Seeds.html')
+def seeds():
+    return render_template('Seeds.html')
 
 @app.route('/buy', methods=['POST'])
 def buy():
---
+++ Website/stock_market/static/script.js
@@ -0,0 +1,64 @@
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementsByName("password")[0];
+    var eyeIcons = document.querySelectorAll(".bi-eye");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        eyeIcons.forEach(function(eyeIcon) {
+            eyeIcon.classList.remove("bi-eye");
+            eyeIcon.classList.add("bi-eye-slash");
+        });
+    } else {
+        passwordInput.type = "password";
+        eyeIcons.forEach(function(eyeIcon) {
+            eyeIcon.classList.remove("bi-eye-slash");
+            eyeIcon.classList.add("bi-eye");
+        });
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var eyeIcons = document.querySelectorAll(".bi-eye");
+    eyeIcons.forEach(function(eyeIcon) {
+        eyeIcon.addEventListener("click", togglePasswordVisibility);
+    });
+
+    var userTypeSelect = document.querySelector('select[name="user_type"]');
+
+    var farmLocationLabel = document.getElementById('farm_location_label');
+    var farmLocationInput = document.getElementById('farm_location_input');
+
+    var farmSizeLabel = document.getElementById('farm_size_label');
+    var farmSizeInput = document.getElementById('farm_size_input');
+
+    var companyNameLabel = document.getElementById('company_name_label');
+    var companyNameInput = document.getElementById('company_name_input');
+
+    function handleUserTypeChange() {
+        var userType = userTypeSelect.value;
+
+        farmLocationInput.style.display = "none";
+        farmLocationLabel.style.display = "none";
+        farmSizeInput.style.display = "none";
+        farmSizeLabel.style.display = "none";
+        companyNameInput.style.display = "none";
+        companyNameLabel.style.display = "none";
+
+        if (userType === "customer") {
+            farmLocationInput.style.display = "block";
+            farmLocationLabel.style.display = "block";
+            farmSizeInput.style.display = "block";
+            farmSizeLabel.style.display = "block";
+        } else if (userType === "stakeholder") {
+            companyNameInput.style.display = "block";
+            companyNameLabel.style.display = "block";
+        }
+    }
+
+    // Add event listener for user type change
+    userTypeSelect.addEventListener('change', handleUserTypeChange);
+
+    // Call the function initially to set the default state
+    handleUserTypeChange();
+});
+
